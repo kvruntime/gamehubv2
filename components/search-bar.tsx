bc1a18f5eb7c2515111c6196cd09de888ac822fa
kvruntime <kvruntime@gmail.com>
2024-09-03 17:15:20 +0000

    feat():add search game features

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -1,8 +1,26 @@
+'use client'
+import { useGamesQueryStore } from "@/lib/store";
 import { Input } from "./ui/input";
+import { throttle } from "throttle-debounce"
+import { useRouter } from "next/navigation";
+
+
 
 function SearchBar() {
+  const setSearchGameText = useGamesQueryStore(s => s.setSearchGameText)
+  const router = useRouter()
+
+  const fetchGame = throttle(1000, (searchGameText: string) => {
+    setSearchGameText(searchGameText)
+    router.push("/")
+  })
+
   return (<div className="grow mx-3">
-  <Input  placeholder="Search game"/>
+    <Input placeholder="Search game" onInput={(e) => {
+      const entry = (e.target as HTMLInputElement)
+      if (entry.value != null)
+        fetchGame(entry.value)
+    }} />
   </div>);
 }
 
